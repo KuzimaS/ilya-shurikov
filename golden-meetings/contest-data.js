d99d4962b23bf8ce8c1408199feeab0b74409dbc
KuzimaS <ilja-shurikov@rambler.ru>
2026-09-04 08:53:48 +0300

    Add golden meetings TV assets

---
+++ golden-meetings/contest-data.js
@@ -0,0 +1,79 @@
+/* Единый опубликованный список участников и шкала наград. */
+window.ContestPeople = [
+    {id:'kuznetsov-anton',name:'Кузнецов Антон',role:'РОП',dept:'Отдел Кузнецова'},
+    {id:'zemlyukov-ilya',name:'Землюков Илья',role:'ст. Менеджер · 462',dept:'Отдел Кузнецова'},
+    {id:'amelchenko-sergey',name:'Амельченко Сергей',role:'Менеджер · 151',dept:'Отдел Кузнецова'},
+    {id:'kuznetsov-alexander',name:'Кузнецов Александр',role:'Менеджер · 158',dept:'Отдел Кузнецова'},
+    {id:'egorov-dmitry',name:'Егоров Дмитрий',role:'Менеджер · 162',dept:'Отдел Кузнецова'},
+    {id:'ufelman-sergey',name:'Уфельман Сергей',role:'Менеджер · 125',dept:'Отдел Кузнецова'},
+    {id:'kurtnizirov-dmitry',name:'Куртнизиров Дмитрий',role:'Менеджер · 252',dept:'Отдел Кузнецова'},
+    {id:'fomenko-roman',name:'Фоменко Роман',role:'Менеджер · 474',dept:'Отдел Кузнецова'},
+
+    {id:'vildanov-salavat',name:'Вильданов Салават',role:'РОП · 396',dept:'Отдел Вильданова'},
+    {id:'nebaykin-sergey',name:'Небайкин Сергей',role:'Ведущ. менеджер · 313',dept:'Отдел Вильданова'},
+    {id:'ilchevsky-oleg',name:'Ильчевский Олег',role:'Ведущ. менеджер · 140',dept:'Отдел Вильданова'},
+    {id:'sadykhova-lyaman',name:'Садыхова Ляман',role:'Ведущ. менеджер · 729',dept:'Отдел Вильданова'},
+    {id:'panasiy-ekaterina',name:'Панасий Екатерина',role:'Ведущ. менеджер · 157',dept:'Отдел Вильданова'},
+    {id:'gradesko-tamara',name:'Градеско Тамара',role:'ст. Менеджер · 511',dept:'Отдел Вильданова'},
+    {id:'kocharovsky-arseny',name:'Кочаровский Арсений',role:'ст. Менеджер · 401',dept:'Отдел Вильданова'},
+    {id:'balakireva-victoria',name:'Балакирева Виктория',role:'ст. Менеджер · 236',dept:'Отдел Вильданова'},
+    {id:'mzhelsky-anton',name:'Мжельский Антон',role:'ст. Менеджер · 127',dept:'Отдел Вильданова'},
+    {id:'donskikh-anna',name:'Донских Анна',role:'Менеджер · 500',dept:'Отдел Вильданова'},
+    {id:'skorobogatova-ekaterina',name:'Скоробогатова Екатерина',role:'Менеджер · 405',dept:'Отдел Вильданова'},
+    {id:'vlasov-nikita',name:'Власов Никита',role:'Менеджер · 805',dept:'Отдел Вильданова'},
+
+    {id:'vostrikov-vladislav',name:'Востриков Владислав',role:'РОП · 333',dept:'Отдел Вострикова'},
+    {id:'boldyrev-dmitry',name:'Болдырев Дмитрий',role:'Ведущ. менеджер · 232',dept:'Отдел Вострикова'},
+    {id:'yashkulova-tsagana',name:'Яшкулова Цагана',role:'Ведущ. менеджер · 348',dept:'Отдел Вострикова'},
+    {id:'bushin-dmitry',name:'Бушин Дмитрий',role:'ст. Менеджер · 449',dept:'Отдел Вострикова'},
+    {id:'artemov-evgeny',name:'Артёмов Евгений',role:'ст. Менеджер · 302',dept:'Отдел Вострикова'},
+    {id:'lazovoy-sergey',name:'Лазовой Сергей',role:'ст. Менеджер · 330',dept:'Отдел Вострикова'},
+    {id:'vasilchenko-ivan',name:'Васильченко Иван',role:'ст. Менеджер · 124',dept:'Отдел Вострикова'},
+    {id:'morev-pavel',name:'Морев Павел',role:'ст. Менеджер · 416',dept:'Отдел Вострикова'},
+    {id:'golovachev-dmitry',name:'Головачёв Дмитрий',role:'Менеджер · 155',dept:'Отдел Вострикова'},
+    {id:'lebedev-artem',name:'Лебедев Артем',role:'Менеджер · 121',dept:'Отдел Вострикова'},
+    {id:'dombrauskaite-victoria',name:'Домбраускайте Виктория',role:'Стажёр · 417',dept:'Отдел Вострикова'},
+
+    {id:'guzhov-mikhail',name:'Гужов Михаил',role:'РОП · 172',dept:'Отдел Гужова'},
+    {id:'barikov-yury',name:'Бариков Юрий',role:'Ведущ. менеджер · 001',dept:'Отдел Гужова'},
+    {id:'demkin-denis',name:'Дёмкин Денис',role:'ст. Менеджер · 132',dept:'Отдел Гужова'},
+    {id:'faybushevich-evgeny',name:'Файбушевич Евгений',role:'ст. Менеджер · 341',dept:'Отдел Гужова'},
+    {id:'semchenko-stepan',name:'Семченко Степан',role:'ст. Менеджер · 384',dept:'Отдел Гужова'},
+    {id:'vakhlaev-daniil',name:'Вахлаев Даниил',role:'Менеджер · 163',dept:'Отдел Гужова'},
+    {id:'mitrochenkov-andrey',name:'Митроченков Андрей',role:'Менеджер · 329',dept:'Отдел Гужова'},
+    {id:'krasikov-konstantin',name:'Красиков Константин',role:'Менеджер · 344',dept:'Отдел Гужова'},
+    {id:'gurov-vitaly',name:'Гуров Виталий',role:'ст. Стажёр · 345',dept:'Отдел Гужова'},
+    {id:'kryuchkova-nadezhda',name:'Крючкова Надежда',role:'Менеджер · 454',dept:'Отдел Гужова'},
+
+    {id:'eremenkov-sergey',name:'Ерёменков Сергей',role:'РОП',dept:'Отдел Ерёменкова'},
+    {id:'demidov-nikita',name:'Демидов Никита',role:'Менеджер · 505',dept:'Отдел Ерёменкова'},
+    {id:'usachev-yury',name:'Усачев Юрий',role:'ст. Стажёр · 461',dept:'Отдел Ерёменкова'},
+    {id:'vorobev-vyacheslav',name:'Воробьев Вячеслав',role:'ст. Стажёр · 510',dept:'Отдел Ерёменкова'},
+
+    {id:'levchenko-artem',name:'Левченко Артем',role:'РОП · 610',dept:'Отдел Левченко'},
+    {id:'kimaev-stanislav',name:'Кимаев Станислав',role:'Ведущ. Менеджер · 506',dept:'Отдел Левченко'},
+    {id:'kosyreva-alena',name:'Косырева Алена',role:'ст. Стажёр · 484',dept:'Отдел Левченко'},
+    {id:'ryzhov-alexander',name:'Рыжов Александр',role:'ст. Стажёр · 512',dept:'Отдел Левченко'},
+
+    {id:'dolzhikov-anton',name:'Должиков Антон',role:'РОА · 700',dept:'Адаптация'},
+    {id:'ostroglyad-sergey',name:'Острогляд Сергей',role:'Стажёр · 501',dept:'Адаптация'},
+    {id:'zholtyak-artem',name:'Жолтяк Артем',role:'Стажёр · 778',dept:'Адаптация'},
+    {id:'smetankina-rimma',name:'Сметанкина Римма',role:'Стажёр · 783',dept:'Адаптация'},
+    {id:'gagarintseva-angelina',name:'Гагаринцева Ангелина',role:'Стажёр · 742',dept:'Адаптация'},
+    {id:'balyuk-kirill',name:'Балюк Кирилл',role:'Стажёр · 691',dept:'Адаптация'},
+    {id:'sinitsin-roman',name:'Синицин Роман',role:'Стажёр · 785',dept:'Адаптация'},
+    {id:'konvisar-darya',name:'Конвисар Дарья',role:'Стажёр · 790',dept:'Адаптация'},
+    {id:'shlenkov-vadim',name:'Шленков Вадим',role:'Стажёр · 847',dept:'Адаптация'},
+    {id:'vafin-vyacheslav',name:'Вафин Вячеслав',role:'Стажёр · 877',dept:'Адаптация'},
+    {id:'novik-igor',name:'Новик Игорь',role:'Стажёр · 894',dept:'Адаптация'},
+    {id:'gromov-alexander',name:'Громов Александр',role:'Стажёр · 887',dept:'Адаптация'},
+    {id:'vagabova-elvira',name:'Вагабова Эльвира',role:'Стажёр · 895',dept:'Адаптация'},
+
+    {id:'shcherbakov-andrey',name:'Щербаков Андрей',role:'ДПИ Менеджер · 455',dept:'Инвестиционный отдел'},
+    {id:'fil-maximilian',name:'Филь Максимилиан',role:'ДПИ Менеджер · 314',dept:'Инвестиционный отдел'},
+    {id:'freiman-elena',name:'Фрейман Елена',role:'ДПИ Менеджер · 502',dept:'Инвестиционный отдел'}
+  ];
+
+window.ContestMilestones=[{n:5,p:10000},{n:7,p:15000},{n:10,p:25000},{n:12,p:30000},{n:15,p:40000},{n:20,p:50000},{n:25,p:60000},{n:30,p:70000}];
+
+window.ContestPublishedState = { assigned: {}, held: {}, updatedAt: null };
